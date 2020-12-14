5d58e03eb1d29a137c501c1fbaa36d7c9037f417
Daniel Chen <danchenzq@gmail.com>
2020-12-14 05:44:18 +0000

    day14

--- 2020/day14.py
+++ 2020/day14.py
@@ -1,8 +1,42 @@
 def solve1(lines):
-    return 0
+    mask = ""
+    mem = {}
+    for line in lines:
+        if line.split()[0] == "mask":
+            mask = line.split()[2]
+        else:
+            asdf = line.split()
+            addr = int(asdf[0][4:-1])
+            val = list(str(bin(int(asdf[2])))[2:].zfill(36))
+            for i in range(36):
+                if mask[i] == "1": val[i] = "1"
+                elif mask[i] == "0": val[i] = "0"
+            val = int(''.join(val),2)
+            mem[addr] = val
+    return sum(mem.values())
 
 def solve2(lines):
-    return 0
+    mask = ""
+    mem = {}
+    for line in lines:
+        if line.split()[0] == "mask":
+            mask = line.split()[2]
+        else:
+            asdf = line.split()
+            addr = list(str(bin(int(asdf[0][4:-1])))[2:].zfill(36))
+            val = int(asdf[2])
+            for i in range(36):
+                if mask[i] == "1": addr[i] = "1"
+                if mask[i] == "X": addr[i] = "0"
+            addr = ''.join(addr)
+            q = [2**(35-i) for i in range(36) if mask[i] == "X"]
+            for i in range(2**len(q)):
+                w = list(map(int,list(str(bin(i))[2:].zfill(len(q)))))
+                e = [j for j in q]
+                for j in range(len(e)): e[j]*=w[j]
+                r = int(addr,2)+sum(e)
+                mem[r] = val
+    return sum(mem.values())
 
 import sys
 lines = [l.rstrip('\n') for l in sys.stdin]

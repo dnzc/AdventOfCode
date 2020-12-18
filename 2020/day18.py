ab79ee1c21ca41c194bd088a6ff8569fe73ae11d
Daniel Chen <danchenzq@gmail.com>
2020-12-18 05:44:00 +0000

    day 18

--- 2020/day18.py
+++ 2020/day18.py
@@ -32,4 +32,4 @@ def solve2(lines):
 import sys
 lines = [l.rstrip('\n') for l in sys.stdin]
 print("\033[96m\033[1m[*] PART 1:", solve1(lines), "\033[0m")
-# print("\033[92m\033[1m[*] PART 2:", solve2(lines), "\033[0m")
+print("\033[92m\033[1m[*] PART 2:", solve2(lines), "\033[0m")
